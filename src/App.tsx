f382c7116f665403ea097438318b8be3fc91bb4e
伊東和成 <itokazunari5375@icloud.com>
2021-10-26 19:08:38 +0900

    フロント側でもデータを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { useAuth0 } from "@auth0/auth0-react";
+import axios from "axios";
+
+interface Post {
+  title: string;
+  caption: string;
+}
 
 function App() {
-  const {
-    isAuthenticated,
-    loginWithRedirect,
-    logout,
-    user,
-    getAccessTokenSilently,
-  } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout, getAccessTokenSilently } =
+    useAuth0();
+
+  const [token, setToken] = useState<string>("");
+
+  const [posts, setPosts] = useState<Post[]>();
+
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({});
+        setToken(accessToken);
+      } catch (e) {
+        alert(e);
+      }
+    };
+    getToken();
+  }, []);
+
+  const fetchPosts = () => {
+    axios.get("http://localhost:3000/api/v1/posts").then((res) => {
+      setPosts(res.data);
+    });
+  };
+
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
+
+  console.log(token);
 
   return (
     <div className="App">
@@ -18,13 +47,23 @@ function App() {
         {!isAuthenticated ? (
           <button onClick={loginWithRedirect}>Log in</button>
         ) : (
-          <button
-            onClick={() => {
-              logout({ returnTo: window.location.origin });
-            }}
-          >
-            Log out
-          </button>
+          <>
+            <h2>投稿一覧</h2>
+            <button onClick={fetchPosts}>投稿取得</button>
+            {posts?.map((post: any, index: number) => (
+              <div key={index}>
+                <p>{post.title}</p>
+                <p>{post.caption}</p>
+              </div>
+            ))}
+            <button
+              onClick={() => {
+                logout({ returnTo: window.location.origin });
+              }}
+            >
+              Log out
+            </button>
+          </>
         )}
         <img src={logo} className="App-logo" alt="logo" />
       </header>
--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 const domain: string = process.env.REACT_APP_AUTH0_DOMAIN || "";
 const clientId: string = process.env.REACT_APP_AUTH0_CLIENT_ID || "";
+const audience: string = process.env.REACT_APP_AUTH0_AUDIENCE || "";
 
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
+    audience={audience}
     redirectUri={window.location.origin}
   >
     <React.StrictMode>

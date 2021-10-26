d31c8a6b85ae2cffb4832f3f3b6814678cdb0cce
伊東和成 <itokazunari5375@icloud.com>
2021-10-26 17:21:26 +0900

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    getAccessTokenSilently,
+  } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
+        {!isAuthenticated ? (
+          <button onClick={loginWithRedirect}>Log in</button>
+        ) : (
+          <button
+            onClick={() => {
+              logout({ returnTo: window.location.origin });
+            }}
+          >
+            Log out
+          </button>
+        )}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
